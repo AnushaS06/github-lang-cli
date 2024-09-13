025a4c7c904424ced2093031a90d8fc18b08ee56
Anusha S <40312715+AnushaS06@users.noreply.github.com>
2024-09-13 09:50:37 +0000

    Fetching the top 5 languages and logging the same

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 export const GITHUB_API_BASE_URL = 'https://api.github.com';
 export const API_RATE_LIMIT_ERROR = 'You have exceeded your GitHub API rate limit.';
 export const INVALID_GIT_ERROR = 'Invalid GitHub profile URL'
+export const NUMBER_OF_LANGUAGES = 5
+export const SUPPORTED_GIT_PROVIDERS = ['github', 'gitlab', 'bitbucket'];
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
 import axios from 'axios';
-import {GITHUB_API_BASE_URL, API_RATE_LIMIT_ERROR, INVALID_GIT_ERROR} from './constants';
+import {GITHUB_API_BASE_URL, API_RATE_LIMIT_ERROR, INVALID_GIT_ERROR, NUMBER_OF_LANGUAGES, SUPPORTED_GIT_PROVIDERS} from './constants';
 
-
-const getLanguages =  async(username: string) =>{
-    const response = await axios.get(`${GITHUB_API_BASE_URL}/users/${username}/repos`)
-    if (response.status === 403) {
+/*
+    Function to fetch repositories from user name
+*/
+const getRepositories =  async(username: string) =>{
+    try{
+        const response = await axios.get(`${GITHUB_API_BASE_URL}/users/${username}/repos`)
+        return response.data;
+    }
+    catch(e){
         throw new Error(API_RATE_LIMIT_ERROR);
     }
-    console.log(response)
-    
-    return response.data;
 }
 
+/*
+    Function to Parse profile name from url
+*/
 export const parseProfileUrl = (url: string): string => {
-    const regex = /github\.com\/([^/]+)/;
+    const providersPattern = SUPPORTED_GIT_PROVIDERS.join('|');
+    const regex = new RegExp(`(?:${providersPattern})\\.com\\/([^/]+)`);
     const match = url.match(regex);
   
     if (!match || match.length < 2) {
@@ -26,14 +32,27 @@ export const parseProfileUrl = (url: string): string => {
 
 const main = async () =>{
     const gitUrl = process.argv[2]
-    console.log(gitUrl)
-
+    
     if(!gitUrl){
         console.error("Please provide a GitHub profile URL.")
         return
     }
     const username = parseProfileUrl(gitUrl);
-    const topLanguages = await getLanguages(username)
+    const repositories = await getRepositories(username)
+    const languageCount: Record<string, number> = {};
+
+    repositories.forEach(repo => {
+        if (repo.language) {
+        languageCount[repo.language] = (languageCount[repo.language] || 0) + 1;
+        }
+    });
+
+    const sortedLanguages = Object.entries(languageCount)
+        .sort((a, b) => b[1] - a[1])
+        .map(entry => entry[0]);
+    
+    const mostUsedLanguages =  sortedLanguages.slice(0, NUMBER_OF_LANGUAGES);
+    console.log('Top 5 languages used:', mostUsedLanguages);
 }
 
 main()
