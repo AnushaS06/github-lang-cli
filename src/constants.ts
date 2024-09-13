dec3112728672e94ee50017dd00dc6f17a0cf3fe
Anusha S <40312715+AnushaS06@users.noreply.github.com>
2024-09-13 09:18:40 +0000

    API integration to fetch all repos of a given github url

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const GITHUB_API_BASE_URL = 'https://api.github.com';
+export const API_RATE_LIMIT_ERROR = 'You have exceeded your GitHub API rate limit.';
+export const INVALID_GIT_ERROR = 'Invalid GitHub profile URL'
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,39 @@
-const main = () =>{
+import axios from 'axios';
+import {GITHUB_API_BASE_URL, API_RATE_LIMIT_ERROR, INVALID_GIT_ERROR} from './constants';
+
+
+const getLanguages =  async(username: string) =>{
+    const response = await axios.get(`${GITHUB_API_BASE_URL}/users/${username}/repos`)
+    if (response.status === 403) {
+        throw new Error(API_RATE_LIMIT_ERROR);
+    }
+    console.log(response)
+    
+    return response.data;
+}
+
+export const parseProfileUrl = (url: string): string => {
+    const regex = /github\.com\/([^/]+)/;
+    const match = url.match(regex);
+  
+    if (!match || match.length < 2) {
+      throw new Error(INVALID_GIT_ERROR);
+    }
+  
+    return match[1];
+  };
+  
+
+const main = async () =>{
     const gitUrl = process.argv[2]
     console.log(gitUrl)
+
+    if(!gitUrl){
+        console.error("Please provide a GitHub profile URL.")
+        return
+    }
+    const username = parseProfileUrl(gitUrl);
+    const topLanguages = await getLanguages(username)
 }
 
 main()
